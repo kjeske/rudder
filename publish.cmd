51bd3dd9dff928085810acb4e60705796688bfba
Krzysztof Jeske <krzysztof.jeske@fabres.pl>
2019-04-25 20:04:34 +0200

    Add pdb to the package.
    Change the versioning pattern to include build number.

--- publish.cmd
+++ publish.cmd
@@ -1,3 +1,4 @@
-del .\src\Projects\Ruddex\bin\Release\*.nupkg
-dotnet pack .\src\Ruddex.sln -c Release
-nuget push .\src\Projects\Ruddex\bin\Release\*.nupkg -Source https://api.nuget.org/v3/index.json
+@ECHO OFF
+del src\bin\Release\*.nupkg
+dotnet pack src -c Release
+nuget push src\bin\Release\*.nupkg -Source https://api.nuget.org/v3/index.json
